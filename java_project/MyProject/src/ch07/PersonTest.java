4bb132bd8f3fa782e1be510984e491be36345b90
ricky9397 <kimzzan1234@gmail.com>
2021-05-26 18:02:25 +0900

    Update PersonTest.java

--- java_project/MyProject/src/ch07/PersonTest.java
+++ java_project/MyProject/src/ch07/PersonTest.java
@@ -2,10 +2,10 @@ package ch07;
 
 public class PersonTest {
 	public static void main(String[] args) {
-		Male male = new Male("홍길동", "950804-1019111", "남자", 180.0f);
-		male.introduction();
+		Person p1 = new Male("홍길동", "950804-1019111", "남자", 180.0f);
+		p1.introduction();
 		
-		Female female = new Female("홍길순", "011204", "여자", 163.5f);
-		female.introduction();
+		Person p2 = new Female("홍길순", "011204", "여자", 163.5f);
+		p2.introduction();
 	}
 }
