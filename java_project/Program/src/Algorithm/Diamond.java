02d2c4d1e73a384855b46c4b21ad77752e6954ea
ricky9397 <kimzzan1234@gmail.com>
2021-05-26 21:32:57 +0900

    별찍기
    별찍기

---
+++ java_project/Program/src/Algorithm/Diamond.java
@@ -0,0 +1,33 @@
+package Algorithm;
+
+public class Diamond {
+
+	public static void main(String[] args) {
+		for (int i = 1; i < 6; i++) {
+			for (int j = i; j < 6; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			for (int j = 0; j < i-1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		for (int i = 1; i < 6; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = i; j < 6; j++) {
+				System.out.print("*");
+			}
+			for (int j = i; j < 6-1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
