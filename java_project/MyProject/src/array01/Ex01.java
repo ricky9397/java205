a035ec483088d7aaca835163ac3036c30efa57cc
ricky9397 <kimzzan1234@gmail.com>
2021-05-27 17:39:06 +0900

    수정
    수정

--- java_project/MyProject/src/array01/Ex01.java
+++ java_project/MyProject/src/array01/Ex01.java
@@ -28,6 +28,7 @@ public class Ex01 {
 		int[] arr = new int[5];
 		
 		for(int i = 0; i < arr.length; i++) {
+			System.out.println(i+1 +"번 점수입력");
 			arr[i] = sc.nextInt();
 		}
 		System.out.println("최하점수:" + minValue(arr));
