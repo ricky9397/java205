b193079fd84da2ae7831647674d7952ebd713b85
ricky9397 <kimzzan1234@gmail.com>
2021-05-27 17:19:53 +0900

    배열문제풀이
    배열문제풀이

--- java_project/MyProject/src/array01/Ex02.java
+++ java_project/MyProject/src/array01/Ex02.java
@@ -2,18 +2,26 @@ package array01;
 
 public class Ex02 {
 
+	public static void addOneDArr(int[] arr, int add) {
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] += add;
+		}
+	}
+	
 	public static void addOneDArr(int[][] arr, int add) {
 		for(int i = 0; i < arr.length; i++) {
 			for(int j = 0; j < arr[i].length; j++) {
 				arr[i][j] += add;
+				System.out.print(arr[i][j] + " ");
 			}
+			System.out.println();
 		}
 	}
 	
 	public static void main(String[] args) {
-		int[][] arr = {{20,30,40},{50,60,70}};
+		int[][] arr = new int[5][5];
 		
-		
+		addOneDArr(arr, 5);
 	}
 
 }
--- java_project/MyProject/src/array01/Ex03.java
+++ java_project/MyProject/src/array01/Ex03.java
@@ -1,11 +1,27 @@
 package array01;
 
 public class Ex03 {
-
+	
+	public static void deployment(int[][] arr) {
+		int tmp;
+		for(int i = 0; i < arr.length; i++) {
+			tmp = arr[0][i];
+			arr[0][i] = arr[2][i];
+			arr[2][i] = arr[1][i];
+			arr[1][i] = tmp;
+		}
+		
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j]+ " ");
+			}
+			System.out.println();
+		}
+	}
+	
 	public static void main(String[] args) {
 		int[][] arr = {{1,2,3},{4,5,6},{7,8,9}};
-		
-		
+		deployment(arr);
 		
 	}
 
