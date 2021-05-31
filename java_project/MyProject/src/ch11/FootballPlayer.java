75676d350295d3f8a970d87f9c8f0c8f7282be1f
ricky9397 <kimzzan1234@gmail.com>
2021-05-31 20:53:30 +0900

    ch11
    ch11 문제풀이 미완성

--- java_project/MyProject/src/ch11/FootballPlayer.java
+++ java_project/MyProject/src/ch11/FootballPlayer.java
@@ -14,6 +14,16 @@ public class FootballPlayer {
 	}
 
 	@Override
+	public boolean equals(Object obj) {
+		if(!(obj instanceof FootballPlayer)) {
+			return false;
+		}
+		FootballPlayer f = (FootballPlayer)obj;
+		return name == f.name;
+	}
+	
+	
+	@Override
 	public String toString() {
 		return "FootballPlayer [name=" + name + ", number=" + number + ", team=" + team + ", age=" + age + "]";
 	}
--- java_project/MyProject/src/ch11/FootballTwo.java
+++ java_project/MyProject/src/ch11/FootballTwo.java
@@ -1,10 +1,39 @@
 package ch11;
 
+import java.util.*;
+
 public class FootballTwo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Set<FootballPlayer> arr = new HashSet<FootballPlayer>();
+		while(true) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("선수이름을 입력하세요 >> ");
+			String name = sc.next();
+			System.out.println("선수 번호를 입력하세요 >> ");
+			int number = sc.nextInt();
+			System.out.println("선수 팀이름을 입력하세요 >> ");
+			String team = sc.next();
+			System.out.println("선수 나이를 입력하세요 >> ");
+			int age = sc.nextInt();
+			
+			FootballPlayer f1 = new FootballPlayer(name, age, team, age);
+			arr.add(f1);
+			FootballPlayer f2 = new FootballPlayer(name, age, team, age);
+			arr.add(f2);
+//			arr.add(new FootballPlayer(name, age, team, age));
+
+			Iterator<FootballPlayer> itr = arr.iterator();
+			while(itr.hasNext()) {
+				FootballPlayer currentStr = itr.next();
+				System.out.println(currentStr);
+			}
+			if(f1.equals(f2)) {
+				System.out.println("이름이 같습니다.");
+				break;
+			}
+		}
 
 	}
-
 }
