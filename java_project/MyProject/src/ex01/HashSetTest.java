0f26309f09c41a1e84748bceef864f2ddb81b863
ricky9397 <kimzzan1234@gmail.com>
2021-06-01 10:19:30 +0900

    HashSet수정
    HashSet수정

--- java_project/MyProject/src/ex01/HashSetTest.java
+++ java_project/MyProject/src/ex01/HashSetTest.java
@@ -2,6 +2,7 @@ package ex01;
 
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 
 public class HashSetTest {
 
@@ -10,19 +11,35 @@ public class HashSetTest {
 		// 중복 저장을 허용하지 않는다.
 		// 저장 순서를 저장하지 않는다.
 		
-		HashSet<String> hashSet = new HashSet<String>();
+		Set<SimpleNumber> hashSet = new HashSet<>();
 		
 		// 데이터 저장
-		hashSet.add("First");
-		hashSet.add("Second");
-		hashSet.add("Third");
-		hashSet.add("First");
+		hashSet.add(new SimpleNumber(10));
+		hashSet.add(new SimpleNumber(20));
+		hashSet.add(new SimpleNumber(20));
 		
 		// 데이터의 일괄 처리
-		Iterator<String> itr = hashSet.iterator();
+		Iterator<SimpleNumber> itr = hashSet.iterator();
 		while(itr.hasNext()) {
 			System.out.println(itr.next());
 		}
 	}
 
+}
+
+class SimpleNumber {
+	
+	int num;
+	
+	SimpleNumber(int num) {
+		this.num = num;
+	}
+
+	@Override
+	public String toString() {
+		return String.valueOf(num);
+	}
+	
+	
+	
 }
