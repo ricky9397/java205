96235d8396de7671da4e02caf8f71a7aa8c32dd4
ricky9397 <kimzzan1234@gmail.com>
2021-06-02 11:54:47 +0900

    input, output Stream
    input, output Stream

--- java_project/Program/src/Algorithm/AddDigits.java
+++ java_project/Program/src/Algorithm/AddDigits.java
@@ -8,7 +8,7 @@ public class AddDigits {
 		Scanner sc = new Scanner(System.in);
 		int s = sc.nextInt();
 		int[] arr = new int[100];
-		
+
 		arr[1] = 1;
 		arr[2] = 1;
 		for(int i=3; i<100; i++) {
@@ -17,14 +17,14 @@ public class AddDigits {
 		for(int i=1; i<=s; i++) {
 			System.out.print(arr[i]+" ");
 		}
-		
+
 		System.out.println();
-	
+
 		int num1, num2, sum;
 		num1=0;  // 첫번째와 두번째 값이 1이어야 하므로 초기값을 0 과
-	    num2=1;  // 1로 준다.
-	    sum=1;   // 첫번째 1은 그냥 초기값으로 설정
-		
+		num2=1;  // 1로 준다.
+		sum=1;   // 첫번째 1은 그냥 초기값으로 설정
+
 		for(int i=0; i<10; i++) {
 			System.out.print(sum+" ");
 			sum=num1+num2;  // 두 값을 더한 후
