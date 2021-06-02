50169b0b7203ce1e038e93ac6cb1d66a5cbfd0c1
ricky9397 <kimzzan1234@gmail.com>
2021-06-02 09:41:48 +0900

    ch11 문제풀이
    ch11 문제풀이

---
+++ java_project/MyProject/src/ch11/FootballTwo.java
@@ -0,0 +1,26 @@
+package ch11;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class FootballTwo {
+
+	public static void main(String[] args) {
+		Set<FootballPlayer> arr = new HashSet<FootballPlayer>();
+		
+		arr.add(new FootballPlayer("이동국", 1, "전북", 43));
+		arr.add(new FootballPlayer("이동국", 2, "전북", 43));
+		arr.add(new FootballPlayer("박지성", 3, "전북", 40));
+		arr.add(new FootballPlayer("손흥민", 4, "서울", 30));
+		arr.add(new FootballPlayer("이청용", 5, "울산", 34));
+		arr.add(new FootballPlayer("이청용", 6, "울산", 34));
+		arr.add(new FootballPlayer("기성용", 7, "서울", 33));
+		
+		Iterator<FootballPlayer> itr = arr.iterator();
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+	}
+
+}
