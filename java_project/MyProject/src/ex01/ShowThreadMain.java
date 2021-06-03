9b5ae74cb3d35725e739b88e83a868ccd7f05d93
ricky9397 <kimzzan1234@gmail.com>
2021-06-03 11:57:51 +0900

    Tread
    Tread

---
+++ java_project/MyProject/src/ex01/ShowThreadMain.java
@@ -0,0 +1,16 @@
+package ex01;
+
+public class ShowThreadMain {
+
+	public static void main(String[] args) {
+		
+		// 스레드 인스턴스 생성
+		ShowThread t1 = new ShowThread("스레드1");
+		ShowThread t2 = new ShowThread("      스레드2");
+		
+		// 스레드의 실행
+		t1.start();
+		t2.start();
+		
+	}
+}
