f43eacd72d4bc564cd4b6399a2a1b2b6fce0cace
ricky9397 <kimzzan1234@gmail.com>
2021-06-07 09:41:07 +0900

    13장 하이로우 수정
    13장 하이로우 수정

--- java_project/MyProject/src/ch13/Game.java
+++ java_project/MyProject/src/ch13/Game.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 
 public class Game {
 
-	public static boolean chk = false;
-	
 	public static void main(String[] args) {
 		CountDownThread ct = new CountDownThread();
 		InputThread it = new InputThread();
@@ -38,10 +36,7 @@ class CountDownThread extends Thread {
 	@Override
 	public void run() {
 		for (int i = 10; i > 0; i--) {
-			if(Game.chk) {
-				return;
-			}
-			System.out.println(i);
+
 			try {
 				sleep(1000);
 			} catch (InterruptedException e) {
@@ -49,7 +44,7 @@ class CountDownThread extends Thread {
 				e.printStackTrace();
 			}
 		}
-		System.out.println("입력시간이 지나 프로그램을 종료합니다.");
+		System.out.println("10초 시간이 지나 프로그램을 종료합니다.");
 		System.exit(0);
 	}
 }
