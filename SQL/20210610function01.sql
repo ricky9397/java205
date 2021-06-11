41828c78be20369fb6009cc38952c8eab61a7c05
ricky9397 <kimzzan1234@gmail.com>
2021-06-11 15:58:00 +0900

    SQL FUNCTION01
    SQL FUNCTION01 수업

--- SQL/20210610function01.sql
+++ SQL/20210610function01.sql
@@ -45,3 +45,74 @@ FROM DUAL;
 
 SELECT SYSDATE -- 년/월/일 날짜 추출
 FROM DUAL;
+
+-----------------------------------------------------------------------------
+-- 2021.06.11 
+-----------------------------------------------------------------------------
+
+-- 형변환 함수 
+-- 날짜 -> 문자
+-- to_char(날짜 데이터, 패턴), to_char(숫자, '패턴')
+SELECT SYSDATE, TO_CHAR(SYSDATE, 'YYYY.MM.DD. HH24:MI:SS')
+FROM DUAL;
+
+SELECT ENAME, HIREDATE, TO_CHAR(HIREDATE, 'YYYY.MM.DD')
+FROM EMP;
+
+SELECT *FROM ORDERS;
+
+SELECT ORDERID, ORDERDATE, TO_CHAR(ORDERDATE, 'YYYY.MM.DD')
+FROM ORDERS;
+
+-- 숫자 -> 문자
+SELECT TO_CHAR(123456, '0000000000'), TO_CHAR(123456, '9999999999')
+FROM DUAL;
+
+SELECT EMPNO, ENAME, SAL, TO_CHAR(SAL, 'L999,999,999')
+FROM EMP;
+
+
+-- '1,000,000' + 100000
+
+-- 문자->숫자
+-- TO_NUBER(문자열, 패턴)
+SELECT TO_NUMBER('1,000,000', '9,999,999'), TO_NUMBER('1,000,000', '9,999,999') + 100000
+FROM DUAL;
+
+-- 문자->날짜
+-- TO_DATE(문자열, 패턴)
+SELECT TO_DATE('2012.05.17', 'YYYY.MM.DD'), TRUNC((SYSDATE-TO_DATE('2012.05.17', 'YYYY.MM.DD'))/365)
+FROM DUAL;
+
+-- DECODE 함수 : 분기를 위해 사용 SWITCH-CASE와 유사하다
+-- DECODE(컬럼, = 조건1 값, 조건1의 참일때 사용할 값
+--          , 조건2 값, 조건2의 참일때 사용할 값
+--          ,.....
+-- )
+
+-- EMP 테이블에서 부서번호에 맞는 부서이름 출력
+SELECT * FROM DEPT;
+
+-- 10 ACCOUNTING
+-- 20 RESEARCH
+-- 30 SALES
+-- 40 OPERATIONS
+
+SELECT ENAME, DEPTNO,
+        DECODE(DEPTNO, 10, 'ACCOUNTING',
+                        20, 'RESEARCH',
+                        30, 'SALES',
+                        40, 'OPERATIONS'
+                        ) AS DNAME
+FROM EMP
+ORDER BY DNAME;
+
+-- 8. 직급에 따라 급여를 인상하도록 하자.
+-- 직급이 'ANALYST'인 사원은 5%, 'SALESMAN'인 사원은 10%, 'MANAGER'인 사원은 15%, 'CLERK'인 사원은 20% 인상한다.
+SELECT ENAME, SAL,
+        DECODE(JOB, 'ANALYST', SAL*1.05, -- 5%인상
+                    'SALESMAN', SAL*1.1, -- 10% UP
+                    'MANAGER', SAL*1.15, -- 15% UP
+                    'CLERK', SAL*1.2 -- 20% UP
+        )
+FROM EMP;
