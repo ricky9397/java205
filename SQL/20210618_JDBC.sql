9cd7e946eb165ae45c7a6c3734c2137db94ce5cf
ricky9397 <kimzzan1234@gmail.com>
2021-06-18 16:38:59 +0900

    JDBC DAO
    JDBC DAO 수업내용

--- SQL/20210618_JDBC.sql
+++ SQL/20210618_JDBC.sql
@@ -9,3 +9,7 @@ insert into dept01 values (dept01_deptno_seq.nextval, 'dev', 'SEOUL');
 create sequence dept01_deptno_seq
 start with 10
 increment by 10;
+
+delete from dept01 where deptno=?;
+
+update dept01 set dname =?, loc=? where deptno=?
