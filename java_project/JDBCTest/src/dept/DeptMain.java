6f24070373346ae690baaf528fb3cc42fd58fc5d
ricky9397 <kimzzan1234@gmail.com>
2021-06-18 18:22:05 +0900

    JDBC DAO
    JDBC DAO  수업내용 싱글톤패턴포함

--- java_project/JDBCTest/src/dept/DeptMain.java
+++ java_project/JDBCTest/src/dept/DeptMain.java
@@ -1,92 +1,58 @@
 package dept;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.Scanner;
 
 public class DeptMain {
 
 	public static void main(String[] args) {
-		Connection conn = null;
 
-		// 1. 드라이버 로드
+		DeptManager manager = new DeptManager(DeptDao.getInstance());
+		
+		Scanner sc = new Scanner(System.in);
+		
 		try {
+			// 1. 드라이버 로드
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-
-			System.out.println("드라이버 로드 성공!");
-
-			// 2. 연결
-			String jdbcUrl = "jdbc:oracle:thin:@localhost:1521:xe";
-			String user = "hr";
-			String pw = "tiger";
-
-			conn = DriverManager.getConnection(jdbcUrl, user, pw);
-			System.out.println("데이터베이스 연결 성공!!!");
-
-			DeptDao dao = new DeptDao();
-
-//			List<Dept> list = dao.getdeptList(conn);
-//
-//			for(Dept dept : list) {
-//				System.out.println(dept);
-//			}
-//			Scanner sc = new Scanner(System.in);
-//			
-//			System.out.println("부서입력을 시작합니다.");
-//			System.out.println("부서번호 부서이름 부서위치 순으로 입력해주세요.");
-//			System.out.println("예) 50 dev seoul");
-//			String deptData = sc.nextLine();
-//			String[] deptDatas = deptData.split(" ");
-//			
-//			// Dept 객체 생성 -> Dao 메소드 호출
-//			Dept dept = new Dept(Integer.parseInt(deptDatas[0]), deptDatas[1], deptDatas[2]);
-//			
-//			int result = dao.insertDept(conn, dept);
-//			if(result > 0) {
-//				System.out.println("입력 성공!");
-//			} else {
-//				System.out.println("입력 실패!");
-//			}
-			
-//			Scanner sc = new Scanner(System.in);
-//
-//			System.out.println("부서수정을 시작합니다.");
-//			System.out.println("수정하고자하는 부서번호 새부서이름 새부서위치 순으로 입력해주세요.");
-//			System.out.println("예) 50 dev seoul");
-//			String deptData = sc.nextLine();
-//			String[] deptDatas = deptData.split(" ");
-//
-//			// Dept 객체 생성 -> Dao 메소드 호출
-//			Dept dept = new Dept(Integer.parseInt(deptDatas[0]), deptDatas[1], deptDatas[2]);
-//
-//			int result = dao.updateDept(conn, dept);
-//			if(result > 0) {
-//				System.out.println("수정 성공!");
-//			} else {
-//				System.out.println("수정 실패!");
-//			}
-			Scanner sc = new Scanner(System.in);
-			System.out.println("부서 정보를 삭제합니다.");
-			System.out.println("삭제할 부서번호를 입력해주세요.");
-			String deptno = sc.nextLine();
-			int result = dao.deleteDept(conn, Integer.parseInt(deptno));
-			
-			if(result > 0) {
-				System.out.println("삭제 완료!");
-			} else {
-				System.out.println("삭제 실패! : 해당 번호의 부서가 존재하지 않습니다.");
+			while(true) {
+				System.out.println("부서관리 프로그램");
+				System.out.println("---------------------------------------");
+				System.out.println("[1]부서리스트");
+				System.out.println("[2]부서정보입력");
+				System.out.println("[3]부서정보수정");
+				System.out.println("[4]부서정보삭제");
+				System.out.println("[5]프로그램종료");
+				System.out.println("---------------------------------------");
+				System.out.println("원하시는 기능의 번호를 입력해주세요.");
+				
+				int num = Integer.parseInt(sc.nextLine());
+				switch(num) {
+				case 1:
+					manager.deptList();
+					break;
+				case 2:
+					manager.inputData();
+					break;
+				case 3:
+					manager.editDept();
+					break;
+				case 4:
+					manager.delDept();
+					break;
+				case 5:
+					System.out.println("프로그램을 종료합니다.");
+					return;
+				default:
+					System.out.println("잘못입력하셧습니다. 다시입력하세요.");
+					break;
+				}
 			}
-			
-			
+
+//			DeptDao dao = DeptDao.getInstance();  // new DeptDao(); -> 싱글톤패턴
+
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
+		} 
 	}
 
 }
