f8b3bfacff0e21be7331a1a84f9f722255d70120
ricky9397 <kimzzan1234@gmail.com>
2021-07-12 23:56:13 +0900

    myproject 공부
    myproject 공부

--- web/myproject/join.js
+++ web/myproject/join.js
@@ -1,3 +1,12 @@
+const toogleBtn = document.querySelector('.navbar_toogleBtn');
+const menu = document.querySelector('.navbar_menu');
+const icons = document.querySelector('.navbar_icons');
+
+toogleBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    icons.classList.toggle('active');
+});
+
 function Member(id, pw, name){
     this.id = id;
     this.pw = pw;
