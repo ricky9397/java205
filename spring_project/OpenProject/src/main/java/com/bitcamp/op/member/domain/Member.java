3065f58a2ae320ede8f902c7ce3e49d56b5769fb
ricky9397 <kimzzan1234@gmail.com>
2021-08-10 00:48:00 +0900

    commit
    commit

--- spring_project/OpenProject/src/main/java/com/bitcamp/op/member/domain/Member.java
+++ spring_project/OpenProject/src/main/java/com/bitcamp/op/member/domain/Member.java
@@ -18,7 +18,6 @@ public class Member {
 		this.membername = username;
 		this.memberphoto = memberphoto;
 	}
-	
 	public Member(int idx, String memberid, String password, String username, String memberphoto, Timestamp regdate) {
 		this.idx = idx;
 		this.memberid = memberid;
@@ -27,10 +26,8 @@ public class Member {
 		this.memberphoto = memberphoto;
 		this.regdate = regdate;
 	}
-
 	public Member() {
 	}
-
 	public int getIdx() {
 		return idx;
 	}
@@ -38,52 +35,40 @@ public class Member {
 	public void setIdx(int idx) {
 		this.idx = idx;
 	}
-
 	public String getMemberid() {
 		return memberid;
 	}
-
 	public void setMemberid(String memberid) {
 		this.memberid = memberid;
 	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
 	public String getMembername() {
 		return membername;
 	}
-
 	public void setMembername(String membername) {
 		this.membername = membername;
 	}
-
 	public Timestamp getRegdate() {
 		return regdate;
 	}
-
 	public void setRegdate(Timestamp regdate) {
 		this.regdate = regdate;
 	}
-
 	public String getMemberphoto() {
 		return memberphoto;
 	}
-
 	public void setMemberphoto(String memberphoto) {
 		this.memberphoto = memberphoto;
 	}
-
 	// java.sql.TimeStamp -> java.util.Date
 	public Date getDate() {
 		return new Date(getRegdate().getTime());
 	}
-
 	@Override
 	public String toString() {
 		return "Member [idx=" + idx + ", memberid=" + memberid + ", password=" + password + ", membername=" + membername
