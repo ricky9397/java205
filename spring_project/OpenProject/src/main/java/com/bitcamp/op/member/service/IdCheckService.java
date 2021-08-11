6a265daef9c223e53e2856014998c6ba84f635e9
ricky9397 <kimzzan1234@gmail.com>
2021-08-11 13:52:21 +0900

    commit

--- spring_project/OpenProject/src/main/java/com/bitcamp/op/member/service/IdCheckService.java
+++ spring_project/OpenProject/src/main/java/com/bitcamp/op/member/service/IdCheckService.java
@@ -7,33 +7,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bitcamp.op.jdbc.ConnectionProvider;
+import com.bitcamp.op.member.dao.JdbctemplateMemberDao;
 import com.bitcamp.op.member.dao.MemberDao;
 
 @Service
 public class IdCheckService {
-	
+
+//	@Autowired
+//	MemberDao dao;
+
 	@Autowired
-	MemberDao dao;
-	
-	
+	private JdbctemplateMemberDao dao;
+
 	public String idCheck(String id) {
-		
+
 		String result = "Y";
-		Connection conn = null;
-		
+		//Connection conn = null;
+
 		try {
-			conn = ConnectionProvider.getConnection();
-			if(dao.selectById(conn, id) > 0) {
+			// conn = ConnectionProvider.getConnection();
+			if (dao.selectById(id) > 0) {
 				result = "N";
 			}
-			
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
 		return result;
 	}
 	
