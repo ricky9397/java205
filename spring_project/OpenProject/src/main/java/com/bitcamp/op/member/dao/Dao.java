fb979b09132b6e6110659ddbca3b25886079e000
ricky9397 <kimzzan1234@gmail.com>
2021-09-04 07:18:36 +0900

    commit

--- spring_project/OpenProject/src/main/java/com/bitcamp/op/member/dao/Dao.java
+++ spring_project/OpenProject/src/main/java/com/bitcamp/op/member/dao/Dao.java
@@ -25,7 +25,6 @@ public interface Dao {
 	int selectTotalCount();
 	// 검색을 통한 회원 리스트
 	List<Member> selectMember(SearchType searchType);
-	
 	// Rest get : idx 값으로 Member 객체 반환
 	Member selectByIdx(int idx);
 	
