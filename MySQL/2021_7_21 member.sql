8651881ce56a9730a6e8f8a6cd0b257b5fae2d22
ricky9397 <kimzzan1234@gmail.com>
2021-09-05 05:34:43 +0900

    Update 2021_7_21 member.sql

--- MySQL/2021_7_21 member.sql
+++ MySQL/2021_7_21 member.sql
@@ -12,15 +12,17 @@ create table project.member (
 );
 
 -- Select
-select * from project.memguestbook_messageguestbook_messagebemembermembermemberr;
+select * from project.member;
 select * from project.member where idx=1;
+select count(*) from project.member where memberid='cool';
 select * from project.member where memberid='cool' and password='1111';
-select * from member;
+
+
 -- insert
 INSERT INTO project.member
 (memberid, password, membername)
 VALUES
-('dddd', '2222', 'COOL');
+('cool123', '1111', 'COOL');
 
 -- update
 update project.member
